80ddc85175b318a9d03b8c5ba974ccd563e3c4b2
Tulsi Narayanan <tulsinarayanan33@gmail.com>
2025-01-21 22:38:35 +0530

    Fifth Commit

---
+++ components/navbar-routes.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { UserButton } from "@clerk/nextjs";
+import { usePathname, useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
+
+const NavbarRoutes = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isPlayerPage = pathname?.includes("/chapter");
+
+  return (
+    <div className="flex gap-x-2 ml-auto">
+      {isTeacherPage || isPlayerPage ? (
+        <Button>
+          <LogOut className="h-4 w-4 mr-2" />
+          Exit
+        </Button>
+      ) : (
+        <Link>
+          <Button>Teacher Mode</Button>
+        </Link>
+      )}
+      <UserButton />
+    </div>
+  );
+};
+
+export default NavbarRoutes;
