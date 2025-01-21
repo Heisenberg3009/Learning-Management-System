8c4650333e8583563d6ce2d1a05ab7d3a2cb09a6
Tulsi Narayanan <tulsinarayanan33@gmail.com>
2025-01-21 12:25:51 +0530

    Third Commit

--- app/(dashboard)/_components/Logo.tsx
+++ app/(dashboard)/_components/Logo.tsx
@@ -1,15 +1,8 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 const Logo = () => {
-  return (
-    <Image
-    height={130}
-    width={130}
-    alt="logo"
-    src="./logo.svg"
-    />
-  )
-}
+  return <Image height={130} width={130} alt="logo" src="./logo.svg" />;
+};
 
-export default Logo
+export default Logo;
