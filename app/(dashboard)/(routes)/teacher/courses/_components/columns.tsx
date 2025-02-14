0cabb4b6dbc3107fa3e8bf690acd5d6822f230b2
Tulsi Narayanan <tulsinarayanan33@gmail.com>
2025-02-14 09:39:07 +0530

    Thirty Fourth Commit

--- app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -1,12 +1,24 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react";
 import { Course } from "@prisma/client";
+import { Button } from "@/components/ui/button";
 
 export const columns: ColumnDef<Course>[] = [
   {
     accessorKey: "title",
-    header: "Title",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Title
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
   },
   {
     accessorKey: "price",
