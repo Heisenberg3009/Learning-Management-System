a3e93fb653f5c648957c311b7699c31a36bd7ee1
Tulsi Narayanan <tulsinarayanan33@gmail.com>
2025-02-24 11:24:42 +0530

    commit#45 adds the progress done so far

---
+++ components/course-card.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import Image from "next/image";
+
+interface CourseCardProps {
+  id: string;
+  title: string;
+  imageUrl: string;
+  chaptersLength: number;
+  price: number;
+  progress: number | null;
+  category: string;
+}
+
+const CourseCard = ({
+  id,
+  title,
+  imageUrl,
+  chaptersLength,
+  price,
+  progress,
+  category,
+}: CourseCardProps) => {
+  return (
+    <Link href={`courses/${id}`}>
+      <div className="group hover:shadow-sm transition overflow-hidden border -rounded-lg p-3 h-full">
+        <div className="relative w-full aspect-video rounded-md overflow-hidden">
+          <Image fill className="object-cover" src={imageUrl} alt={title} />
+        </div>
+      </div>
+    </Link>
+  );
+};
+export default CourseCard;
