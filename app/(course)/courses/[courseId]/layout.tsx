71bccff6531b902ec9d2ad4975a2144bc8abd0cd
Tulsi Narayanan <tulsinarayanan33@gmail.com>
2025-02-27 13:29:38 +0530

    #50 - adds the progress completed so far

--- app/(course)/courses/[courseId]/layout.tsx
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -4,6 +4,7 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { getProgress } from "@/actions/get-progress";
 import CourseSidebar from "./_components/course-sidebar";
+import CourseNavbar from "./_components/course-navbar";
 
 const Layout = async ({
   children,
@@ -47,9 +48,12 @@ const Layout = async ({
 
   return (
     <div className="h-full">
+      <div className="h-[80px] md:pl-80 fixed inset-y-0 w-full z-50">
+        <CourseNavbar course={course} progressCount={progressCount} />
+      </div>
       <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
         <CourseSidebar course={course} progressCount={progressCount} />
-        <main className="md:pl-80 h-full">{children}</main>
+        <main className="md:pl-80 pt-[80px] h-full">{children}</main>
       </div>
     </div>
   );
