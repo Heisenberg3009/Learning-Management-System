d1d435291a507c8b2eb690841c6af8c9a81f3423
Tulsi Narayanan <tulsinarayanan33@gmail.com>
2025-02-28 08:50:53 +0530

    #54 - adds the progress completed so far

--- actions/get-chapter.ts
+++ actions/get-chapter.ts
@@ -1,3 +1,5 @@
+import { db } from "@/lib/db";
+
 interface GetChapterProps {
   userId: string;
   courseId: string;
@@ -6,6 +8,32 @@ interface GetChapterProps {
 
 const getChapter = async ({ userId, courseId, chapterId }: GetChapterProps) => {
   try {
+    const purchase = await db.purchase.findUnique({
+      where: {
+        userId_courseId: {
+          userId,
+          courseId,
+        },
+      },
+    });
+    const course = await db.course.findUnique({
+      where: {
+        isPublished: true,
+        id: courseId,
+      },
+      select: {
+        price: true,
+      },
+    });
+    const chapter = await db.chapter.findUnique({
+      where: {
+        isPublished: true,
+        id: chapterId,
+      },
+    });
+    if(!chapter || !course)  {
+        throw new Error("Chapter or Course not found.");
+      }
   } catch (error) {
     console.log("[GET_CHAPTER]", error);
     return {
